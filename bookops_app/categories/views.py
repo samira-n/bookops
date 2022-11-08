81ebfb2fcbd3e8f07f7690a7f6bf46836137106b
snabi <s.nabievaaa@gmail.com>
2022-11-08 17:56:41 +0300

    changes category

---
+++ bookops_app/categories/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+
+from categories.models import Category
+from categories.serializers import CategorySerializer
+
+
+class CategoryViewSet(ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
